f254508aab81158f09de1d1457a67bf7efd4d3b3
Gileno <gilenoduarte.jobs@gmail.com>
2025-10-19 14:56:05 -0300

    feat: Added  repository layer

---
+++ python-service/src/repository/movie.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+from models.movie import Movie
+class MovieInterface(ABC):
+    @abstractmethod
+    def create_movie(self, movie) -> Movie:
+        """Saves the movie data and returns the movie object."""
+        pass
+    def update_movie(self,data, id) -> Movie:
+        """Updates the movie data and returns the movie object."""
+        pass
+
+    def delete_movie(self, id)-> str:
+        """Delete the movie and returns a message."""
+        pass
---
+++ python-service/src/repository/movie_repository_impl.py
@@ -0,0 +1,9 @@
+
+from movie import MovieInterface
+from models.movie import Movie
+
+class MovieRepository(MovieInterface):
+
+    def create_movie(self, movie) -> Movie:
+        pass
+    

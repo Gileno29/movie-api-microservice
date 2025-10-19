6202ea27a4d4ecbdca04f3d28ee620753e39296c
Gileno <gilenoduarte.jobs@gmail.com>
2025-10-19 14:54:07 -0300

    feat: Add model of python service

---
+++ python-service/src/models/movie.py
@@ -0,0 +1,8 @@
+class Movie():
+
+    def __init__(self, id, name, description, genre, year):
+        self.id=id
+        self.name=name
+        self.description=description
+        self.genre=genre
+        self.year= year
